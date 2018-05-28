f5ac3c602a7f95e6616946c17259d4add08b3fcf
StefanGabor <stefan.sympax@gmail.com>
2018-05-28 20:06:41 +0200

    Buscar Cliente (Segun un criterio) (Empleado)

--- BancaOnline-war/src/java/managedBeans/BuscarClientes.java
+++ BancaOnline-war/src/java/managedBeans/BuscarClientes.java
@@ -25,11 +25,33 @@ public class BuscarClientes {
     private ClienteFacade clienteFacade;
     
     protected List<Cliente> clientes;
-
+    protected String valor;
+    protected String criterio;
+    protected String [] criterios = {"--Choose Critery--","DNI", "Nombre", "Apellidos", "Entidad", "Oficina","Nº Cuenta"};
     /**
      * Creates a new instance of BuscarUsuario
      */
     public BuscarClientes() {
+    }
+
+    public String[] getCriterios() {
+        return criterios;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getCriterio() {
+        return criterio;
+    }
+
+    public void setCriterio(String criterio) {
+        this.criterio = criterio;
     }
 
     public List<Cliente> getClientes() {
@@ -40,8 +62,37 @@ public class BuscarClientes {
         this.clientes = clientes;
     }
     
+    public String searchByCriteria(){
+        switch(criterio){
+               case "DNI":
+                   clientes = clienteFacade.BuscarPorDNI(valor);
+                   break;
+                case "Nombre":
+                   clientes = clienteFacade.BuscarPorNombre(valor);
+                   break;
+                case "Apellidos":
+                   clientes = clienteFacade.BuscarPorApellido(valor);
+                   break;
+                case "Entidad":
+                   clientes = clienteFacade.BuscarPorEntidad(valor);
+                   break;
+                case "Oficina":
+                   clientes = clienteFacade.BuscarPorOficina(valor);
+                   break;
+                case "Nº Cuenta":
+                   clientes = clienteFacade.BuscarPorNumeroCC(valor);
+                   break;
+           } 
+        return "index";
+    }
+    
     @PostConstruct
     public void init(){
-        clientes = clienteFacade.findAll();
+        if(criterio == null || criterio.equals(criterios[0])){
+            clientes = clienteFacade.findAll(); //inicialmente se muestran todos
+        }else{
+           searchByCriteria();
+        }
+        
     }
 }
